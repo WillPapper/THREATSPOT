3ec175c474741b136413bb46f99413990258b68a
William Papper <contact@papper.me>
2015-04-19 12:53:03 -0500

    Removed debugging code, added console.log()

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 function onRouteGoClick() {
   $("#route").show();
-  console.log("Waiting for timeout");
-  setTimeout(advanceMiles("fa-arrow-circle-up", 2, "fa-arrow-circle-right", 9, "ISIL Stronghold"), 5000);
-  console.log("Timeout 1 finished");
-  setTimeout(advanceMiles("fa-arrow-circle-up", 1, "fa-arrow-circle-right", 8, "ISIL Stronghold"), 10000);
-  console.log("Timeout 2 finished");
+  setTimeout(function(){advanceMiles("fa-arrow-circle-up", 2, "fa-arrow-circle-right", 9, "ISIL Stronghold")}, 5000);
+  setTimeout(function(){advanceMiles("fa-arrow-circle-up", 1, "fa-arrow-circle-right", 8, "ISIL Stronghold")}, 10000);
 }
 
 function advanceMiles(milesDirection, milesAway, threatDirection, threatMilesAway, threatType) {
-  alert("Test");
+  console.log("Timeout started");
   $("#miles-direction").removeClass("fa-arrow-circle-up fa-arrow-circle-down fa-arrow-circle-left fa-arrow-circle-right");
   $("#miles-direction").addClass(milesDirection);
   $("#miles-away").val(milesAway);
