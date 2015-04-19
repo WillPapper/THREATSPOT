dbbe791cb0a3539894edf23342d245e8b90cd838
William Papper <contact@papper.me>
2015-04-19 13:43:42 -0500

    Added the capability to rotate arrows

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function onRouteGoClick() {
   $("#route").show();
-  setTimeout(function(){advanceMiles("fa-arrow-circle-up", "2 miles", "fa-arrow-circle-right", "9 miles", "ISIL Stronghold")}, 5000);
-  setTimeout(function(){advanceMiles("fa-arrow-circle-up", "1 mile", "fa-arrow-circle-right", "8 miles", "ISIL Stronghold")}, 10000);
+  setTimeout(function(){advanceMiles("fa-arrow-circle-up rotate-45-left", "2 miles", "fa-arrow-circle-right", "9.5 miles", "ISIL Stronghold")}, 5000);
+  setTimeout(function(){advanceMiles("fa-arrow-circle-up rotate-45-left", "1 mile", "fa-arrow-circle-right", "9 miles", "ISIL Stronghold")}, 10000);
 }
 
 function advanceMiles(milesDirection, milesAway, threatDirection, threatMilesAway, threatType) {
   console.log("Timeout started");
   console.log($("#miles-direction").attr("class").split(' '));
-  $("#miles-direction").removeClass("fa-arrow-circle-up fa-arrow-circle-down fa-arrow-circle-left fa-arrow-circle-right");
+  $("#miles-direction").removeClass("fa-arrow-circle-up fa-arrow-circle-down fa-arrow-circle-left fa-arrow-circle-right rotate-45-left rotate-45-right");
   $("#miles-direction").addClass(milesDirection);
   $("#miles-away").text(milesAway);
-  $("#threat-direction").removeClass("fa-arrow-circle-up fa-arrow-circle-down fa-arrow-circle-left fa-arrow-circle-right");
+  $("#threat-direction").removeClass("fa-arrow-circle-up fa-arrow-circle-down fa-arrow-circle-left fa-arrow-circle-right rotate-45-left rotate-45-right");
   $("#threat-direction").addClass(threatDirection);
   $("#threat-miles-away").text(threatMilesAway);
   $("#threat-type").text(threatType);
